2986d8f6e12038c3ab4b633a573bcadcf2408b6a
Davi Luca <davi333luca@gmail.com>
2026-04-15 13:18:11 -0300

    pronto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $owner = "Davi Luca";
+    $contributor = null
+?>
+
+<h1>Pull Request em Dupla</h1>
+
+<?php
+    echo '<h2 style="color:green">Dono: ',$owner.'</h2>'
+?>
+
+<h2>Contribuinte:
+
+<?php
+    echo $contributor
+?>
+
+</h2>
